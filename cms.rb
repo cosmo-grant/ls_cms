6b72a9ea38ab080661095c6fd2860f709a47895a
Cosmo Grant <cosmodgrant@gmail.com>
2022-06-23 12:47:48 -0700

    assignment 4

--- cms.rb
+++ cms.rb
@@ -8,3 +8,9 @@ get '/' do
   @filenames = Dir.glob('*', base: root + '/data')
   erb :index
 end
+
+get '/:filename' do
+  file_path = root + '/data/' + params[:filename]
+  headers['Content-Type'] = 'text/plain'
+  File.read(file_path)
+end
